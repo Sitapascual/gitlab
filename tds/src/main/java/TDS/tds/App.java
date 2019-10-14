632e663f90b5bf954fcd07be5195d4ed9fc95d41
Alumno <Alumno@155.54.223.236>
2019-10-14 18:35:26 +0200

    Hola

---
+++ tds/src/main/java/TDS/tds/App.java
@@ -0,0 +1,13 @@
+package TDS.tds;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
